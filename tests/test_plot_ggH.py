a0957f04814fced0b6c8885b01361e9c0302f4ee
Sebastian Hornung <sebastian.hornung@cern.ch>
2021-02-18 08:46:18 +0100

    add regression test for skim.log and histograms

---
+++ tests/test_plot_ggH.py
@@ -0,0 +1,32 @@
+#
+# python script to test the histograms
+#
+
+import sys
+import ROOT
+
+# list of histograms which should exist (only a selection for now)
+required_keys = ['ggH_pt_1', 'ggH_pt_2']
+
+file = ROOT.TFile.Open('hist_ggH.root')
+keys = [k.GetName() for k in file.GetListOfKeys()]
+
+print('\n'.join(keys))
+for required_key in required_keys:
+    if not required_key in keys:
+        print(f'Required ROOT object key not found {required_key}')
+        syst.exit(1)
+
+
+expected_integral_pt_1 = 222.88716647028923
+
+# test value of the integral of the ggH_pt_1 histogram
+# check if difference is a small number to allow for rounding effects
+integral = file.ggH_pt_1.Integral()
+if abs(integral-expected_integral_pt_1) > 0.0001:
+    print(f'Integral of ggH_pt_1 is different: {integral}')
+    sys.exit(2)
+
+expected_number_of_bins = 30
+assert file.ggH_pt_2.GetNbinsX() == expected_number_of_bins, \
+    f'Number of bins of ggH_pt_2 differs from the expected value of {expected_number_of_bins}'
